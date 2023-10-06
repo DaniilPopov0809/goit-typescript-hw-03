577bd3cfe45a6972e9040166dbefbfb35b98efdf
Daniil Popov <x6uhrox@gmail.com>
2023-10-06 17:27:56 +0300

    ADD: task

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+
+  constructor(protected key: Key) {}
+
+  public tenants: Person[] = [];
+
+  public comeIn(person: Person) {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
+
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
